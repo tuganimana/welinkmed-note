b8a9e8a436767c8dd376b5648320c0493010f773
Telesphore <tuganimana01@gmail.com>
2021-07-01 13:35:36 +0200

    starting

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+// eslint-disable-next-line no-use-before-define
+import React from 'react'
+import './App.css'
+import './css/tailwind.css'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Home from './components/home'
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <>
+    <Router>
+      <Switch>
+        <Route exact path=''>
+          <Home/>
 
-export default App;
+        </Route>
+      </Switch>
+    </Router>
+    </>
+  )
+}
+export default App
---
+++ src/components/home.tsx
@@ -0,0 +1,11 @@
+
+// eslint-disable-next-line no-use-before-define
+import React from 'react'
+
+export default function Home () {
+  return (
+        <>
+     <h1>Home</h1>
+        </>
+  )
+}
