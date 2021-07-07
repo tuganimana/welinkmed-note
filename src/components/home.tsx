3d01bbda0b35d62cfb480b2a30d752a0d39cad06
Telesphore <tuganimana01@gmail.com>
2021-07-07 13:06:31 +0200

    Actions

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,11 +1,12 @@
 
 // eslint-disable-next-line no-use-before-define
 import React from 'react'
-
+import Alert from './alerts'
 export default function Home () {
   return (
         <>
      <h1>Home</h1>
+     <Alert message="Failed to create an account"/>
         </>
   )
 }
