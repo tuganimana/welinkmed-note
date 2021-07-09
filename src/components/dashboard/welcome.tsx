082d0350c0e46f6e32ddb0ae02d264bc90e16138
annicklinca <lincaannick14@gmail.com>
2021-07-09 12:44:54 +0200

    new one

--- src/components/dashboard/welcome.tsx
+++ src/components/dashboard/welcome.tsx
@@ -2,6 +2,15 @@
 import React from 'react'
 export default function Welcome () {
   return (<>
-  <p>Welcome page</p>
+  {/* <p>Welcome page</p> */}
+      <div className="bg-white">
+        <div className="font-semibold text-md">
+          Resident Info<span className="text-gray-400 text-sm">/PERU</span>
+        </div>
+       <div className="form-group row">
+          <div className="col-sm-2"><label>Username</label></div>
+          <div className="col-sm-8"><label>Username</label></div>
+       </div>
+       </div>
         </>)
 }
