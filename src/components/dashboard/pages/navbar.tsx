a219a5979a239b43692cc7e3f86b5fe348718ad7
philbert250 <ndzphilbert@gmail.com>
2021-07-09 12:43:17 +0200

    correct nav bar

--- src/components/dashboard/pages/navbar.tsx
+++ src/components/dashboard/pages/navbar.tsx
@@ -1,14 +1,15 @@
 // eslint-disable-next-line no-use-before-define
 import React from 'react'
 import Sidebar from '../Sidebar'
+import Topbar from './Topbar'
 export default function Dashboard (props: any) {
   return (
         <div className="lg:flex w-full bg-lightBlue gap-5">
             <div className="lg:w-1/5">
                 <Sidebar/>
             </div>
-            {/* main body */}
             <div className="lg:w-4/5 bg-green-100">
+                <Topbar/>
                 {props.children}
             </div>
         </div>
