b4c13f397de13eb75f58b6c3382141418ca21a87
Telesphore <tuganimana01@gmail.com>
2021-07-26 20:51:20 +0200

    feat: WIP resident add

--- src/components/dashboard/pages/overview.tsx
+++ src/components/dashboard/pages/overview.tsx
@@ -1,9 +1,12 @@
 // eslint-disable-next-line no-use-before-define
-import React from 'react'
+import React, { useState } from 'react'
 import { Select } from 'antd'
+import { useForm } from 'react-hook-form'
+import { ResidentType } from '../../../utils/types'
 const { Option } = Select
-
 export default function Overview () {
+  const { register, handleSubmit, formState: { errors } } = useForm<ResidentType>()
+  const [attendingPhysician, setAttending] = useState('')
   const children = []
   for (let i = 10; i < 36; i++) {
     children.push(<Option value={i.toString(36) + i} key={i.toString(36) + i}>{i.toString(36) + i}</Option>)
@@ -12,41 +15,50 @@ export default function Overview () {
   function handleChange (value:any) {
     console.log(`selected ${value}`)
   }
+  function handleAttending (value:any) {
+    setAttending(value)
+  }
+  const registerResident =(data:any) =>{
+
+  }
   return (
         <>
          <div className="p-4 bg-white rounded-xl shadows-xl mx-4">
     <div className="grid md:grid-cols-2 gap-4">
           <div className="p-2">
             <label className="text-md">Firstname</label>
-            <input type="text" name="" className="w-full p-2 border"/>
+            <input type="text" {...register('firstName', { required: '* This field is required' })} name="" className="w-full p-2 border"/>
+            <span className="text-red-600 text-xs">{errors.firstName && errors.firstName.message}</span>
           </div>
           <div className="p-2">
             <label>Lastname</label>
-            <input type="text" name="" className="w-full p-2 border"/>
+            <input type="text" {...register('lastName', { required: 'This field is required' })} className="w-full p-2 border"/>
+            <span className="text-red-600 text-xs">{errors.lastName && errors.lastName.message}</span>
           </div>
           <div className="p-2">
             <label>Resident State</label>
-            <select className="w-full p-2 border">
+            <select {...register('residentSate')}className="w-full p-2 border">
               <option>Select here</option>
-              <option>Newyork</option>
-              <option>Atlanta</option>
-              <option>Maine</option>
-              <option>Texas</option>
-              <option>Georgia</option>
-              <option>Mossachussets</option>
+              <option value="Newyork">Newyork</option>
+              <option value="Antlanta">Atlanta</option>
+              <option value="Maine">Maine</option>
+              <option value="Texas">Texas</option>
+              <option value="Georgia">Georgia</option>
+              <option value="Massachussets">Massachussets</option>
             </select>
           </div>
           <div className="p-2">
             <label> Religion</label>
-            <input type="text" name="" className="w-full p-2 border" />
+            <input type="text" {...register('religion')} className="w-full p-2 border" />
           </div>
           <div className="p-2">
             <label>Maritial Status</label>
-            <select className="w-full p-2 border">
+            <select {...register('maritialStatus')} className="w-full p-2 border">
               <option>Select here</option>
-              <option>Single</option>
-              <option>Divorced</option>
-              <option>None of above</option>
+              <option value="Single">Single</option>
+              <option value="Divorced">Divorced</option>
+              <option value="Married">Married</option>
+              <option value="None of Above">None of above</option>
             </select>
           </div>
           <div className="p-2">
@@ -55,25 +67,18 @@ export default function Overview () {
           </div>
           <div className="p-2">
             <label>Attending Physician</label>
-            <Select
-      mode="multiple"
-      allowClear
-      style={{ width: '100%' }}
-      placeholder="Please select"
-      defaultValue={['Aron10']}
-      onChange={handleChange}
-    >
-      {children}
-    </Select>
+            <Select mode="tags" style={{ width: '100%' }} placeholder="Attending Physician" onChange={handleAttending}>
+    {children}
+  </Select>
           </div>
           <div className="p-2">
             <label>Added Date</label>
-            <input type="date" className="w-full p-2 border"/>
+            <input type="date" {...register('addedDate')} className="w-full p-2 border"/>
           </div>
           <div className="p-2">
             <label>Additional Physician</label>
             <Select
-      mode="multiple"
+      mode="tags"
       allowClear
       style={{ width: '100%' }}
       placeholder="Please select"
