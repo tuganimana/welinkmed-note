935b7713565af69b6890e29ec2cea7a26b6cdbc9
Telesphore <tuganimana01@gmail.com>
2021-07-31 16:25:44 +0200

    feat:  made c hange on welcome

--- src/App.tsx
+++ src/App.tsx
@@ -56,7 +56,7 @@ function App () {
             <Medicationdue/>
           </Dashboard>
         </Route>
-        <Route path="/resident-info/:residentid">
+        <Route path="/resident-infos/:residentid">
           <Dashboard>
             <ResidentInfo/>
           </Dashboard>
